c9440e4b59d1c7e644c8d702182bc4a9dd0a1445
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-03-23 16:28:21 -0600

    adds functions including closure

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activityString = "roller-skate") {
+    return (`This Saturday, I want to ${activityString}!`);
+}
+
+function mondayWork(activityString = "go to the office") {
+    return (`This Monday, I will ${activityString}.`);
+}
+
+function wrapAdjective(visualFlare = "*") {
+    return (function (adjective = "special") {
+        return (`You are ${visualFlare}${adjective}${visualFlare}!`)
+    })
+}
